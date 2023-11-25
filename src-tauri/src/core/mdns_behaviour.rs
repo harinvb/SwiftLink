215cfbd206ea22cbc9f308950dbf876ae6c9b101
Hari Babu Nalluri <harinvb@gmail.com>
2023-11-25 21:09:32 +0530

    wip: work items

--- src-tauri/src/core/mdns_behaviour.rs
+++ src-tauri/src/core/mdns_behaviour.rs
@@ -11,8 +11,13 @@ pub fn process_mdns_event(_context: Context, event: Event, swarm: &mut SLSwarm) 
         Event::Discovered(peers) => {
             for (peer_id, multiaddr) in peers {
                 //TODO: Initiate info exchange with peer
-
-
+                info!("discovered peer: {:?} with multiaddr: {:?}", peer_id, multiaddr);
+                if let Err(e) = swarm.dial(&peer_id) {
+                    info!("error dialing peer: {:?}, error: {}", peer_id, e);
+                }
+                else {
+                    info!("dialed peer: {:?}", peer_id);
+                }
                 //TODO: Add peer and info to local db
             }
         }
