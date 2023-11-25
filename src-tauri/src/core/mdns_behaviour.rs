2577c03aa77b6fdaedb3cacea9fe2b9ad8b6dc4c
Hari Babu Nalluri <harinvb@gmail.com>
2023-11-25 21:19:24 +0530

    wip: work items

--- src-tauri/src/core/mdns_behaviour.rs
+++ src-tauri/src/core/mdns_behaviour.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
+
 use libp2p::mdns::Event;
 use libp2p::swarm::dial_opts::DialOpts;
 use tracing::{error, info};
 
 use crate::core::{Context, SLSwarm};
-use crate::core::json_behaviour::Request;
 
 pub type Mdns = libp2p::mdns::tokio::Behaviour;
 
@@ -16,17 +16,19 @@ pub fn process_mdns_event(_context: Context, event: Event, swarm: &mut SLSwarm) 
                 //TODO: Initiate info exchange with peer
                 info!("discovered peer: {:?} with multiaddr: {:?}", peer_id, multiaddr);
                 if !peer_map.contains_key(&peer_id) {
-                    peer_map.insert(&peer_id, vec![]);
+                    peer_map.insert(peer_id.clone(), vec![]);
                 }
-                peer_map.get(&peer_id).unwrap().push(multiaddr);
+                match peer_map.get_mut(&peer_id) {
+                    Some(vec) => vec.push(multiaddr),
+                    None => error!("error adding multiaddr to peer map"),
+                }
                 //TODO: Add peer and info to local db
             }
 
             for (peer_id, multiaddrs) in peer_map {
-               if let Err(e) = swarm.dial(DialOpts::peer_id(peer_id.clone()).addresses(multiaddrs).build()) {
-                   error!("error dialing peer: {:?}, error: {}", peer_id, e);
-               }
-                else {
+                if let Err(e) = swarm.dial(DialOpts::peer_id(peer_id.clone()).addresses(multiaddrs).build()) {
+                    error!("error dialing peer: {:?}, error: {}", peer_id, e);
+                } else {
                     info!("dialed peer: {:?}", peer_id);
                 }
             }
