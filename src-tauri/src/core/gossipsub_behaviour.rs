419e39c7a3cf664e47ab0e2c26b191e8bb6628ab
Hari Babu Nalluri <harinvb@gmail.com>
2023-11-26 14:58:40 +0530

    wip: work items

--- src-tauri/src/core/gossipsub_behaviour.rs
+++ src-tauri/src/core/gossipsub_behaviour.rs
@@ -1,4 +1,5 @@
 use libp2p::gossipsub::{Behaviour, Event};
+use tracing::{info, warn};
 use crate::core::behaviour::SLSwarm;
 use crate::core::Context;
 
@@ -8,9 +9,17 @@ pub type Gossipsub = Behaviour;
 
 pub fn process_gossipsub_event(_context: Context, event: Event, _swarm: &mut SLSwarm) {
     match event {
-        Event::Message { .. } => {}
-        Event::Subscribed { .. } => {}
-        Event::Unsubscribed { .. } => {}
-        Event::GossipsubNotSupported { .. } => {}
+        Event::Message { message_id,message,propagation_source } => {
+            info!("Got message: {} from {}", String::from_utf8_lossy(&message.data), propagation_source);
+        }
+        Event::Subscribed { peer_id,topic } => {
+            info!("Peer {} subscribed to topic {}", peer_id, topic.as_str());
+        }
+        Event::Unsubscribed { peer_id,topic } => {
+            info!("Peer {} unsubscribed from topic {}", peer_id, topic.as_str());
+        }
+        Event::GossipsubNotSupported { peer_id } => {
+            warn!("Peer {} does not support gossipsub", peer_id);
+        }
     }
 }
